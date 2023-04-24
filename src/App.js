2ba6eeae016fbaa613d93fab9fa549667ea4c0ce
Empairim <118220478+Empairim@users.noreply.github.com>
2023-04-24 13:20:12 -0400

    is this code going in the correct direction?

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import axios from "axios";
+
+const API_BASE_URL =
+  "http://ip172-19-0-7-ch3a122e69v000epu0d0-8077.direct.labs.play-with-docker.com"; // I THINK this is the correct base url
+
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(`${API_BASE_URL}/signup`, { email, password });
+      alert("User Registered Successfully");
+    } catch (err) {
+      alert("Error registering user");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email Address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`${API_BASE_URL}/authenticate`, {
+        email,
+        password,
+      });
+      const token = response.data.token;
+      console.log("Token:", token);
+      alert("Logged in successfully.");
+    } catch (error) {
+      alert("Error logging in.");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {" "}
+      {/*this wraps the app and provides routing functionality*/}
+      <Routes>
+        {" "}
+        {/* this switches the routes when you have multiple components grouped together */}
+        <Route path="/signup" component={Register} />
+        <Route path="/login" component={Login} />
+      </Routes>
+    </Router>
   );
 }
 
